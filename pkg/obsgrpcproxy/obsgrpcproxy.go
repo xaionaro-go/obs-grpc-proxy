e200afd8581cc64d8a538b06a0be1622e2622612
Dmitrii Okunev <xaionaro@dx.center>
2024-08-18 19:21:42 +0100

    Remove double importing of the same package

--- pkg/obsgrpcproxy/obsgrpcproxy.go
+++ pkg/obsgrpcproxy/obsgrpcproxy.go
@@ -9,29 +9,28 @@ import (
 
 	goobs "github.com/andreykaipov/goobs"
 	"github.com/xaionaro-go/obs-grpc-proxy/protobuf/go/obs_grpc"
-	obsgrpc "github.com/xaionaro-go/obs-grpc-proxy/protobuf/go/obs_grpc"
 )
 
 type GetClientFunc func() (*goobs.Client, context.CancelFunc, error)
 
 type Proxy struct {
-	obsgrpc.UnimplementedOBSServer
+	obs_grpc.UnimplementedOBSServer
 
 	GetClient GetClientFunc
 }
 
-var _ obsgrpc.OBSServer = (*Proxy)(nil)
+var _ obs_grpc.OBSServer = (*Proxy)(nil)
 
 type ProxyAsClient Proxy
 
-var _ obsgrpc.OBSClient = (*ProxyAsClient)(nil)
+var _ obs_grpc.OBSClient = (*ProxyAsClient)(nil)
 
 type ClientAsServer struct {
 	obs_grpc.UnimplementedOBSServer
 	obs_grpc.OBSClient
 }
 
-var _ obsgrpc.OBSServer = (*ClientAsServer)(nil)
+var _ obs_grpc.OBSServer = (*ClientAsServer)(nil)
 
 func New(getClient GetClientFunc) *Proxy {
 	return &Proxy{
@@ -43,41 +42,41 @@ func ptr[T any](in T) *T {
 	return &in
 }
 
-func AnyGo2Protobuf(in any) *obsgrpc.Any {
-	var result obsgrpc.Any
+func AnyGo2Protobuf(in any) *obs_grpc.Any {
+	var result obs_grpc.Any
 	switch in := in.(type) {
 	case []byte:
-		result.Union = &obsgrpc.Any_String_{String_: in}
+		result.Union = &obs_grpc.Any_String_{String_: in}
 	case string:
-		result.Union = &obsgrpc.Any_String_{String_: []byte(in)}
+		result.Union = &obs_grpc.Any_String_{String_: []byte(in)}
 	case int:
-		result.Union = &obsgrpc.Any_Integer{Integer: int64(in)}
+		result.Union = &obs_grpc.Any_Integer{Integer: int64(in)}
 	case uint:
-		result.Union = &obsgrpc.Any_Integer{Integer: int64(in)}
+		result.Union = &obs_grpc.Any_Integer{Integer: int64(in)}
 	case int64:
-		result.Union = &obsgrpc.Any_Integer{Integer: int64(in)}
+		result.Union = &obs_grpc.Any_Integer{Integer: int64(in)}
 	case uint64:
-		result.Union = &obsgrpc.Any_Integer{Integer: int64(in)}
+		result.Union = &obs_grpc.Any_Integer{Integer: int64(in)}
 	case int32:
-		result.Union = &obsgrpc.Any_Integer{Integer: int64(in)}
+		result.Union = &obs_grpc.Any_Integer{Integer: int64(in)}
 	case uint32:
-		result.Union = &obsgrpc.Any_Integer{Integer: int64(in)}
+		result.Union = &obs_grpc.Any_Integer{Integer: int64(in)}
 	case int16:
-		result.Union = &obsgrpc.Any_Integer{Integer: int64(in)}
+		result.Union = &obs_grpc.Any_Integer{Integer: int64(in)}
 	case uint16:
-		result.Union = &obsgrpc.Any_Integer{Integer: int64(in)}
+		result.Union = &obs_grpc.Any_Integer{Integer: int64(in)}
 	case int8:
-		result.Union = &obsgrpc.Any_Integer{Integer: int64(in)}
+		result.Union = &obs_grpc.Any_Integer{Integer: int64(in)}
 	case uint8:
-		result.Union = &obsgrpc.Any_Integer{Integer: int64(in)}
+		result.Union = &obs_grpc.Any_Integer{Integer: int64(in)}
 	case float64:
-		result.Union = &obsgrpc.Any_Float{Float: float64(in)}
+		result.Union = &obs_grpc.Any_Float{Float: float64(in)}
 	case float32:
-		result.Union = &obsgrpc.Any_Float{Float: float64(in)}
+		result.Union = &obs_grpc.Any_Float{Float: float64(in)}
 	case bool:
-		result.Union = &obsgrpc.Any_Bool{Bool: in}
+		result.Union = &obs_grpc.Any_Bool{Bool: in}
 	case map[string]any:
-		result.Union = &obsgrpc.Any_Object{
+		result.Union = &obs_grpc.Any_Object{
 			Object: ToAbstractObject(in),
 		}
 	default:
@@ -86,25 +85,25 @@ func AnyGo2Protobuf(in any) *obsgrpc.Any {
 	return &result
 }
 
-func AnyProtobuf2Go(in *obsgrpc.Any) any {
+func AnyProtobuf2Go(in *obs_grpc.Any) any {
 	switch in := in.Union.(type) {
-	case *obsgrpc.Any_Integer:
+	case *obs_grpc.Any_Integer:
 		return in.Integer
-	case *obsgrpc.Any_Float:
+	case *obs_grpc.Any_Float:
 		return in.Float
-	case *obsgrpc.Any_String_:
+	case *obs_grpc.Any_String_:
 		return string(in.String_)
-	case *obsgrpc.Any_Bool:
+	case *obs_grpc.Any_Bool:
 		return in.Bool
-	case *obsgrpc.Any_Object:
+	case *obs_grpc.Any_Object:
 		return FromAbstractObject[map[string]any](in.Object)
 	default:
 		panic(fmt.Errorf("unexpected type: %T", in))
 	}
 }
 
-func ToAbstractObjects[T any](in []T) []*obsgrpc.AbstractObject {
-	result := make([]*obsgrpc.AbstractObject, 0, len(in))
+func ToAbstractObjects[T any](in []T) []*obs_grpc.AbstractObject {
+	result := make([]*obs_grpc.AbstractObject, 0, len(in))
 	for _, item := range in {
 		result = append(result, ToAbstractObject(item))
 	}
@@ -137,11 +136,11 @@ func ptrInt64ToInt(in *int64) *int {
 	return &i
 }
 
-func ToAbstractObject[T any](in T) *obsgrpc.AbstractObject {
+func ToAbstractObject[T any](in T) *obs_grpc.AbstractObject {
 	return toAbstractObjectViaJSON(in)
 }
 
-func toAbstractObjectViaJSON[T any](in T) *obsgrpc.AbstractObject {
+func toAbstractObjectViaJSON[T any](in T) *obs_grpc.AbstractObject {
 	b, err := json.Marshal(in)
 	if err != nil {
 		panic(err)
@@ -152,8 +151,8 @@ func toAbstractObjectViaJSON[T any](in T) *obsgrpc.AbstractObject {
 		panic(err)
 	}
 
-	result := &obsgrpc.AbstractObject{
-		Fields: map[string]*obsgrpc.Any{},
+	result := &obs_grpc.AbstractObject{
+		Fields: map[string]*obs_grpc.Any{},
 	}
 	for k, v := range m {
 		result.Fields[k] = AnyGo2Protobuf(v)
@@ -161,11 +160,11 @@ func toAbstractObjectViaJSON[T any](in T) *obsgrpc.AbstractObject {
 	return result
 }
 
-func FromAbstractObject[T any](in *obsgrpc.AbstractObject) T {
+func FromAbstractObject[T any](in *obs_grpc.AbstractObject) T {
 	return fromAbstractObjectViaJSON[T](in)
 }
 
-func fromAbstractObjectViaJSON[T any](in *obsgrpc.AbstractObject) T {
+func fromAbstractObjectViaJSON[T any](in *obs_grpc.AbstractObject) T {
 	var result T
 	if in == nil || in.Fields == nil {
 		return result
